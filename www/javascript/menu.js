36a9c4fa785ffb390e1106af438ff242f7e3aab1
Will <wstechjunior@gmail.com>
2020-11-09 20:39:43 -0300

    If added this commit will bring a menu with responsiveness to index.html

---
+++ www/javascript/menu.js
@@ -0,0 +1,11 @@
+//MENU RESPONSIVO
+
+let show =  true
+
+const menuSection = document.querySelector(".menu-section")
+const menuToggle = menuSection.querySelector(".menu-toggle")
+
+menuToggle.addEventListener("click", () => {
+	menuSection.classList.toggle("on", show)
+	show = !show
+})
